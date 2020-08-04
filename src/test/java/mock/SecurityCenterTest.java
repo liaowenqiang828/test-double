5d86892d19abf1f5e0b3b7fe5361a50f43936344
liaowenqiang828 <18840965351@163.com>
2020-08-04 19:18:34 +0800

    [Wenqiang Liao] feat: complete test using stub

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -4,16 +4,23 @@ package mock;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+class MockDoorPanel extends DoorPanel {
+
+}
+
 public class SecurityCenterTest {
     /* 需求描述：
-    编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
+    编写SecurityCenter类的单元测试，单元测试switchOn方法，
+    不依赖于DoorPanel的close的方法实现
     * */
 
     @BeforeEach
     public void setUp() {
+
     }
 
     @Test
     public void shouldVerifyDoorIsClosed() {
+
     }
 }
--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -1,15 +1,38 @@
 package stub;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StubGradeSystem extends GradeSystem {
+    @Override
+    public List<Double> gradesFor(long studentId) {
+        return Arrays.asList(88.0, 92.0, 90.0);
+    }
+}
 
 public class GradeServiceTest {
     /* 需求描述：
     编写GradeService类的单元测试，单元测试calculateAverageGrades方法
     * */
+    private StubGradeSystem stubGradeSystem;
+    private GradeService gradeService;
+
+    @BeforeEach
+    public void setUp() {
+        stubGradeSystem = new StubGradeSystem();
+        gradeService = new GradeService(stubGradeSystem);
+    }
 
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-        //Assertions.assertEquals(90.0, result);
+        double result;
+        result = gradeService.calculateAverageGrades(9999999L);
+        assertEquals(90, result);
     }
 }
